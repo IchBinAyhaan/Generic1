b3ae22d47e3fb4cbf2d7111317f0cd189f0f937d
IchBinAyhaan <165468545+IchBinAyhaan@users.noreply.github.com>
2024-05-31 13:02:31 +0400

    first commit

--- Generic/Custom/Program.cs
+++ Generic/Custom/Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        CustomList<int> myList = new CustomList<int>();
+
+        
+        myList.Add(10);
+        myList.Add(20);
+        myList.Add(30);
+
+       
+        Console.WriteLine("Is 20 in the list? " + myList.Contains(20)); 
+        Console.WriteLine("Is 40 in the list? " + myList.Contains(40)); 
+
+       
+        Console.WriteLine("Is the list empty? " + (myList.Any(x => true) ? "No" : "Yes")); 
+
+      
+        Console.WriteLine("First element of the list: " + myList.FirstOrDefault());
+
+       
+        Console.WriteLine("Last element of the list: " + myList.LastOrDefault()); 
+
+        
+        Console.WriteLine("Element at index 1: " + myList.ElementAtOrDefault(1));
+        Console.WriteLine("Element at index 5: " + myList.ElementAtOrDefault(5));
+
+      
+        myList.Clear();
+
+        
+        Console.WriteLine("Is the list empty now? " + (myList.Any(x => true) ? "No" : "Yes")); 
+    }
+}
