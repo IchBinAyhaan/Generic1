5ea473e938a4e5084b73dabea08f4e3068a2c90a
IchBinAyhaan <165468545+IchBinAyhaan@users.noreply.github.com>
2024-05-31 13:01:07 +0400

    first commit

--- Generic/Custom/CustomList.cs
+++ Generic/Custom/CustomList.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Custom
 {
-    internal class CustomList
+   public class CustomList<T> : IEnumerable<T>
+{
+    private List<T> items = new List<T>();
+
+    public void Add(T item)
     {
+        items.Add(item);
     }
+
+    public void Remove(T item)
+    {
+        items.Remove(item);
+    }
+
+    public bool Contains(T item)
+    {
+        return items.Contains(item);
+    }
+
+    public bool Any(Func<T, bool> predicate)
+    {
+        return items.Any(predicate);
+    }
+
+    public void Clear()
+    {
+        items.Clear();
+    }
+
+    public T FirstOrDefault()
+    {
+        return items.FirstOrDefault();
+    }
+
+    public T ElementAtOrDefault(int index)
+    {
+        if (index < 0 || index >= items.Count)
+            return default(T);
+
+        return items[index];
+    }
+
+    public T LastOrDefault()
+    {
+        return items.LastOrDefault();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return items.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
 }
